681742f793f5091cccdb75810008f6a51fb3eab9
theChaosCoder <thechaoscoder@gmail.com>
2019-04-24 20:10:33 +0200

    Show the last 3 years in Calender View

--- inc/lab_data.php
+++ inc/lab_data.php
@@ -6,7 +6,7 @@ require_once 'app.php';
 switch($_GET['id']) {
      case 1:
 		#, COUNT(DISTINCT(`auth`)) AS players
-		$database->query('SELECT COUNT(*) AS Comparison_Type, DATE_FORMAT(connect_date, "%Y%m%d") AS Date FROM `'.DB_TABLE_PA.'` WHERE '.getServerIpsSql(false, '', 'AND').' (year(connect_date) = "2016" OR year(connect_date) = "2015" OR year(connect_date) = "2014") GROUP BY YEAR(connect_date), month(connect_date), day(connect_date) ORDER BY `Date` ASC');
+		$database->query('SELECT COUNT(*) AS Comparison_Type, DATE_FORMAT(connect_date, "%Y%m%d") AS Date FROM `'.DB_TABLE_PA.'` WHERE '.getServerIpsSql(false, '', 'AND').' (year(connect_date) = date("Y") OR year(connect_date) = ((int)date("Y") - 1) OR year(connect_date) = ((int)date("Y") - 2)) GROUP BY YEAR(connect_date), month(connect_date), day(connect_date) ORDER BY `Date` ASC');
 		$data = $database->resultset();
 
 		echo json_encode($data);
